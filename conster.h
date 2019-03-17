d640ffcfb9ce8483421889860d695f370e42f031
avleensm <avleensm@stud.ntnu.no>
2019-03-17 14:20:32 +0100

    Added header file 'conster.h' to the project. Included it in 'main.cpp' to see if I could access the data from header file in main

---
+++ conster.h
@@ -0,0 +1,5 @@
+#pragma once
+
+//Dette er kun en test og kan slettes
+const int MIN = 0;
+const int MAX = 100;
--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 
 #include <iostream>
+#include "conster.h"					//	Test
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hei alle sammem!\n";
+
+
+	//Test to check if I can access data fron 'conster.h':
+	std::cout << "\nThese are the numbers from 'conster.h: " << MIN << " and " << MAX << std::endl;
     return 0;
 }
