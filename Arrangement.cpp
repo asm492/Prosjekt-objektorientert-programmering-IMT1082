59d2b90c24b05c02c94a875b4ed4a8bf080c5ddb
Sindrohj <sindrohj@stud.ntnu.no>
2019-03-18 16:45:51 +0100

    Begynnt på classen

--- Arrangement.cpp
+++ Arrangement.cpp
@@ -1,9 +1,2 @@
-//
-//  Arrangement.cpp
-//  ProsjektFil
-//
-//  Created by Sindre Hjelle on 18/03/2019.
-//  Copyright © 2019 Sindre Hjelle. All rights reserved.
-//
 
-#include "Arrangement.hpp"
+#include "Arrangement.h"
