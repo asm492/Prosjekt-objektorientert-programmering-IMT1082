f2a2e5fe62a3174a4bccd4eae9000e6adf5e388b
avleensm <avleensm@stud.ntnu.no>
2019-03-19 00:30:06 +0100

    Tror jeg har funnet en løsning på 2D dynamic array

--- Stoler.h
+++ Stoler.h
@@ -7,7 +7,43 @@
 class Stoler : public Sone {
 private:
 	int seatConfig[MAXCONFIG + 1][MAXCONFIG + 1];
-	int 
+	int seatPrRow, rows;
+	
+	/*************  2D dynamic array  - DON'T DELETE: ***********/
+	/*
+	cout << "Hvor mange? Sete pr rad * Rad "; cin >> seatPrRow >> rows;
+
+	int** A = new int*[seatPrRow];
+
+	for (int i = 0; i < seatPrRow; i++)
+	{
+		A[i] = new int[rows];
+	}
+
+	for (int i = 0; i < seatPrRow; i++)					//	REMOVE, FILLS RANDOM
+	{
+		for (int j = 0; j < rows; j++)
+		{
+			A[i][j] = (rand() % 100) + 1;
+		}
+
+	}
+
+	for (int i = 0; i < seatPrRow; i++)
+	{
+		for (int j = 0; j < rows; j++)
+			std::cout << A[i][j] << " ";
+
+		cout << endl;
+	}
+
+	// deallocate memory using delete[] operator
+	for (int i = 0; i < seatPrRow; i++)
+		delete[] A[i];
+
+	delete[] A;
+	*/
+
 
 		/*
 	int** a = new int*[rowCount];
