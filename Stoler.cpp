a8a3854247a7c2c9ab5a2f60fe22797825bc59d8
avleensm <avleensm@stud.ntnu.no>
2019-03-18 19:55:12 +0100

    Laget noen nye filer

---
+++ Stoler.cpp
@@ -0,0 +1,12 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif 
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include "Stoler.h"
+
+using namespace std;
+
+
---
+++ Stoler.h
@@ -0,0 +1,34 @@
+#ifndef Stoler_h
+#define Stoler_h
+
+#include "conster.h"
+#include "Sone.h"
+
+class Stoler : public Sone {
+private:
+	int seatConfig[MAXCONFIG + 1][MAXCONFIG + 1];
+	int 
+
+		/*
+	int** a = new int*[rowCount];
+	for(int i = 0; i < rowCount; ++i)
+    a[i] = new int[colCount];
+
+	new:
+	int **ary = new int*[sizeY];
+	for(int i = 0; i < sizeY; ++i) {
+	ary[i] = new int[sizeX];
+
+	slette:
+	for(int i = 0; i < sizeY; ++i) {
+	delete [] ary[i];
+}
+delete [] ary;
+}
+	*/
+public:
+
+};
+
+#endif 
+
