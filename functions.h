839cefa66eb24b0a68cdeafc83ec41354beb5890
Sindrohj <sindrohj@stud.ntnu.no>
2019-03-18 14:03:25 +0100

    Fjernet conster.h

--- functions.h
+++ functions.h
@@ -8,11 +8,11 @@
 
 #ifndef functions_h
 #define functions_h
-#include "conster.h"
+
 
 void read(const char t[], char s[], const int LEN);
 char read();
-void printError(char t[])
+void printError(char t[]);
 int  read(const char t[], const int min, const int max);
 void printMenu();
 int dayNumber(int dd, int mm, int yyyy);
