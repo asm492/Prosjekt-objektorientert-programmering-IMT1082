66c43a12aaa88dddfd45a7a20d9c98a60b67b813
avleensm <avleensm@stud.ntnu.no>
2019-03-18 16:46:12 +0100

    Lagt til nye conster, laget constructor og display() for kunde

--- conster.h
+++ conster.h
@@ -1,4 +1,7 @@
-#pragma once
+
+#ifndef conster_h
+#define conster_h
+
 
 //Dette er kun en test og kan slettes
 const int MINTLF = 10000000;
@@ -6,3 +9,8 @@ const int MAXTLF = 99999999;
 const int MAXCONFIG = 5;				//	Maximum seat-configurations
 const int MINZIP = 1;					//	Norwegian postal-codes 0001-9991
 const int MAXZIP = 9991;
+const int STRLEN = 80;			
+
+
+
+#endif 
--- enums.h
+++ enums.h
@@ -1,3 +1,6 @@
-#pragma once
+#ifndef enums_h
+#define enums_h
 
 enum eventType { Musikk, Sport, Teater, Show, Kino, Familie, Festival };
+
+#endif enums_h
