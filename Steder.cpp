8a125548d406740f8ed9af164d2c4a4be2cfa904
Sindrohj <sindrohj@stud.ntnu.no>
2019-03-29 10:53:38 +0100

    Ingen endring

--- Steder.cpp
+++ Steder.cpp
@@ -41,13 +41,33 @@ void Steder::newVenue() {
     
     tempVenue = new Sted(tempName);
     venueList->add(tempVenue);
+
+	cout << "\n\n\t\tVENUE CREATED!\n";
     
 }
 void Steder::venueDisplay() {
-    /*THIS IS FOR TESING ONLY*/
-    char venueName[STRLEN];
-    read("WHICH VENUE TO DISPLAY?", venueName, STRLEN);
-    venueList->displayElement(venueName);
+	/*THIS IS FOR TESING ONLY*/
+	char venueName[STRLEN];
+	
+	if ((venueList->noOfElements()) > 0)
+	{
+		read("WHICH VENUE TO DISPLAY?", venueName, STRLEN);
+		if (venueList->inList(venueName)) {
+			venueList->displayElement(venueName);
+		}
+		else {
+			cout << "\n\n\t\tNO VENUES IN THE LIST WITH NAME: '";
+			cout << venueName << "'!\n\n";
+		}
+	}
+	else
+	{
+		printError("NO VENUES IN LIST YET!");
+	}
+
+
+	
+	
     
     /************************/
 }
@@ -68,6 +88,52 @@ void Steder::writeVenuesToFile() {
         venueList->add(tempVenue);
     }
     
+}
+void Steder::layoutDisplay()
+{
+	Sted* tmpVenue;
+	char buffer[STRLEN];
+
+	read("Which venue's layout would you like to display?", buffer, STRLEN);
+
+	if (venueList->inList(buffer))
+	{
+		tmpVenue = (Sted*)venueList->remove(buffer);
+		venueList->add(tmpVenue);
+		venueList->displayElement(buffer);
+
+		tmpVenue->displayLayouts();					//	Calls Sted class' function
+	}
+	else
+		cout << "\n\n\t\t'" << buffer << "' IS NOT IN LIST!\n\n";
+}
+void Steder::layoutNew()
+{
+	//tmpVenue->newLayout();
+
+	Sted* tmpVenue;
+	char buffer[STRLEN];
+
+	read("Which venue would you like to add layout to?", buffer, STRLEN);
+
+	if (venueList->inList(buffer))
+	{
+		tmpVenue = (Sted*)venueList->remove(buffer);
+		venueList->add(tmpVenue);
+		venueList->displayElement(buffer);
+		tmpVenue->newLayout();
+	}
+	else
+		cout << "\n\n\t\t'" << buffer << "' IS NOT IN LIST!\n\n";
+	
+}
+void Steder::layoutEdit()
+{
+	//TO DO
+}
+void Steder::layoutDelete()
+{
+	//TO DO 
 }
 void Steder::readVenuesFromFile() {
     
@@ -92,8 +158,30 @@ void Steder::readVenuesFromFile() {
     
     
 }
-void Steder::layoutMenu() {
-    Sted* tempVenue;
+void Steder::layoutMenu() {		//	From main press 0
+	char command;
+	
+	if ((venueList->noOfElements()) > 0)
+	{
+		command = read();
+
+		switch (command)
+		{
+		case 'D': layoutDisplay();	break;
+		case 'N': layoutNew();		break;
+		case 'E': layoutEdit();		break;
+		case 'S': layoutDelete();	break;
+		
+		
+		default:
+			break;
+		}
+	}
+	else
+		printError("VENUE LIST IS EMPTY. YOU MUST ADD A VENUE IN ORDER TO CONFIGURE OR DISPLAY A LAYOUT!");
+
+	
+	/*Sted* tempVenue;
     char venueName[STRLEN];
     read("ENTER NAME OF VENUE", venueName, STRLEN);
     
@@ -105,6 +193,9 @@ void Steder::layoutMenu() {
     }
     else
         printError("DIDN'T FIND ANY VENUES WITH THAT NAME");
-    
+    */
+}
+int Steder::retLastUsedVenue() {
+	return lastUsedVenue;
 }
 
